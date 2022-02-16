2ce18572a19f50bafa6cb44cf9a36b9721f2f7c8
Rebeca Belin <becabelin@gmail.com>
2022-02-15 21:53:57 -0300

    puxando para o github

---
+++ primeiro dia.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+distancia = float(input("Digite a distância em km: "))
+velocidade_media = float(input("Digite a velocidade média em km/h: "))
+tempo_total_horas = distancia / velocidade_media
+
+print(f"\nO tempo total de viagem é de {tempo_total_horas:.2f} horas\n")
