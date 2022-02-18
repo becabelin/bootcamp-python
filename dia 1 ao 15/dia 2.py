ac9ad5fae2d53cccee07c6d89d82f242b47e9baf
Rebeca Belin <becabelin@gmail.com>
2022-02-17 21:49:15 -0300

    dia 2

--- dia 1 ao 15/dia 2.py
+++ dia 1 ao 15/dia 2.py
@@ -2,16 +2,20 @@
 Introdução à sintaxe e tipos de dados
 '''
 
+# Desafio 1
 aluno = input("Digite o nome do aluno: ")
 
 nota1 = float(input("Digite a primeira nota: "))
-
 nota2 = float(input("Digite a segunda nota: "))
-
 nota3 = float(input("Digite a terceira nota: "))    
 
-media_float = (nota1 + nota2 + nota3) / 3
+media = (nota1 + nota2 + nota3) / 3
+media_arred = round(media, 2)
 
-media_int = round(media_float)
+print(f"\nO(a) aluno(a) {aluno} teve a média {media_arred}")
 
-print(f"\nO(a) aluno(a) {aluno} teve a média {media_int}")
+# Desafio 2
+nome = input("Digite o seu nome: ")
+letra = input("Digite uma letra: ")
+
+print(f"Seu nome é {nome}, nele existem {len(nome)} letras e {nome.count(letra)} delas são '{letra}'")
