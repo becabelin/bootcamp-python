484c5327131294f3994db147124db86c811b03f6
Rebeca Belin <becabelin@gmail.com>
2022-02-24 19:16:21 -0300

    dia 3

--- dia 1 ao 15/dia 3.py
+++ dia 1 ao 15/dia 3.py
@@ -10,3 +10,8 @@ print(f"A primeira letra do nome é {aluno[0]}") # 0 é o primeiro caractere
 print(f"A última letra do nome é {aluno[-1]}") # -1 é o último caractere
 print(f"A primeira palavra do nome é {aluno.split()[0]}") # split() separa a string em uma lista
 
+# Dica da tia Fer: para armazenar valores, podem ser usadas variáveis, listas e tuplas.
+# Uma variável guarda um único valor; listas e tuplas podem guardar vários valores.
+# As listas podem ser manipuladas (pode-se inserir elementos, remover elementos, ordenar a lista, ...).
+# As tuplas apenas guardam os valores, sem a possibilidade de ter seus elementos alterados.
+
